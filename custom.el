e4baa23d201d72df0740fc246db22518a981b86c
brenoafb <breno.afb@gmail.com>
2019-10-05 12:10:55 -0300

    Update config.

--- custom.el
+++ custom.el
@@ -5,10 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
  '(custom-enabled-themes (quote (manoj-dark)))
- '(package-selected-packages (quote (evil undo-tree use-package))))
+ '(custom-safe-themes
+   (quote
+    ("a77ced882e25028e994d168a612c763a4feb8c4ab67c5ff48688654d0264370c" default)))
+ '(org-agenda-files (quote ("~/Dropbox/org/index.org")))
+ '(package-selected-packages
+   (quote
+    (haml-mode markdown-mode hindent hindent-mode haskell-mode helm-projectile helm projectile magit flycheck company which-key soothe-theme evil-collection evil undo-tree use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -3,6 +3,9 @@
 ;;; https://sanemacs.com   ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(setq gc-cons-threshold 50000000)
+(setq large-file-warning-threshold 100000000)
+
 ;;; Disable menu-bar, tool-bar, and scroll-bar.
 (if (fboundp 'menu-bar-mode)
     (menu-bar-mode -1))
@@ -40,11 +43,13 @@
 (show-paren-mode 1)                       ; Show closing parens by default
 (setq linum-format "%4d ")                ; Prettify line number format
 (add-hook 'prog-mode-hook                 ; Show line numbers in programming modes
-          (if (fboundp 'display-line-numbers-mode)
-              #'display-line-numbers-mode
-            #'linum-mode))
+	  (if (fboundp 'display-line-numbers-mode)
+	      #'display-line-numbers-mode
+	    #'linum-mode))
 (use-package undo-tree                    ; Enable undo-tree, sane undo/redo behavior
   :init (global-undo-tree-mode))
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
 
 ;;; Keybindings
 (global-set-key (kbd "C->") 'indent-rigidly-right-to-tab-stop) ; Indent selection by one tab length
@@ -73,14 +78,103 @@
 (setq create-lockfiles nil)
 
 ;;; Load wheatgrass as the default theme if one is not loaded already
-
 (if (not custom-enabled-themes)
     (load-theme 'wheatgrass t))
+
+;; font
+(set-face-attribute 'default nil
+		    :family "xos4 Terminus"
+		    :height 90
+		    :weight 'normal
+		    :width 'normal)
 
 ;; evil
 (use-package evil
   :ensure t
-  :init (evil-mode 1))
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
 
-;; font size
-(set-face-attribute 'default nil :height 90)
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+;; soothe theme
+(use-package soothe-theme
+  :ensure t
+  :init (load-theme 'soothe t))
+
+(use-package which-key
+  :ensure t
+  :diminish which-key-mode
+  :config
+  (which-key-mode +1))
+
+(use-package company
+  :ensure t
+  :diminish company-mode
+  :config
+  (add-hook 'after-init-hook #'global-company-mode))
+
+(use-package flycheck
+  :ensure t
+  :diminish flycheck-mode
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+(use-package magit
+  :bind (("C-M-g" . magit-status)))
+
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :bind
+  (("C-c p f" . helm-projectile-find-file)
+   ("C-c p p" . helm-projectile-switch-project)
+   ("C-c p s" . projectile-save-project-buffers))
+  :config
+  (projectile-mode +1))
+
+ (use-package helm
+   :ensure t
+   :defer 2
+   :bind
+   ("M-x" . helm-M-x)
+   ("C-x C-f" . helm-find-files)
+   ("M-y" . helm-show-kill-ring)
+   ("C-x b" . helm-mini)
+   :config
+   (require 'helm-config)
+   (helm-mode 1)
+   (setq helm-split-window-inside-p t
+     helm-move-to-line-cycle-in-source t)
+   (setq helm-autoresize-max-height 0)
+   (setq helm-autoresize-min-height 40)
+   (helm-autoresize-mode 1)
+   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+   (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+   (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+   )
+
+(use-package helm-projectile
+  :ensure t
+  :config
+  (helm-projectile-on))
+
+
+(use-package haskell-mode
+  :ensure t)
+
+(use-package hindent
+  :ensure t
+  :after haskell-mode
+  :config
+  (add-hook 'haskell-mode-hook #'hindent-mode))
+
+(use-package markdown-mode
+  :ensure t)
+;; init.el ends here
